c34fcc2f83a4e21f8029ee9246102fb7e312d902
sekimosan <persephone.0930@gmail.com>
2021-03-11 19:19:23 +0900

    Userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
+  validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\u{30fc}]+\z/}
+  validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\u{30fc}]+\z/}
+  validates :nickname, presence: true
+  validates :birthday, presence: true       
 end
