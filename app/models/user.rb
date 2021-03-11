eb77f728ff9982e65b112198df9d15f42a7fe594
sekimosan <persephone.0930@gmail.com>
2021-03-11 22:57:05 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥]/+\z}
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end       
   validates :last_name_kana, presence: true, format: {with: /\A[\p{katakana}\u{30fc}]+\z/}
   validates :first_name_kana, presence: true, format: {with: /\A[\p{katakana}\u{30fc}]+\z/}
   validates :nickname, presence: true
