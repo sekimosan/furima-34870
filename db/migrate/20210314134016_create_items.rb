be630e79368ed72dc07d56001b1bbc9d5cec3831
sekimosan <persephone.0930@gmail.com>
2021-03-15 12:13:06 +0900

    itemsテーブルとActiveStorage導入

--- db/migrate/20210314134016_create_items.rb
+++ db/migrate/20210314134016_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer  :shipping_fee_id   , null: false
       t.text   :item_pr  , null: false
       t.references  :user  , foreign_key: true
-      t.intger  :item_status_id  , null: false
+      t.integer  :item_status_id  , null: false
       t.integer  :prefecture_id  , null: false
       t.integer  :shipping_day_id  , null: false
       t.integer  :category_id  , null: false
