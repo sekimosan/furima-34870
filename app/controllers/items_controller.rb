c30ea4cef4f276c3fc6285fc35ef2d430875224f
sekimosan <persephone.0930@gmail.com>
2021-03-17 18:09:42 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new ,:edit,:create, :update,:destroy]
   before_action :define_item, only: [:show,:edit,:update,:destroy]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :move_to_index, only: [:edit, :update,:destroy]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end 
