1041620d93acbdeeb17cf4c19e263a3253bfa830
sekimosan <persephone.0930@gmail.com>
2021-03-19 23:40:47 +0900

    OrderAddressクラス単体テストコード

---
+++ spec/factories/order_address.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order_address do
+    postal_code {"123-4567"}
+    prefecture_id {Faker::Number.between(from: 1,to: 47)}
+    city {"朝日市"}
+    house_number {"長山1-1"}
+    building {"ハイツ長山"}
+    phone_number {"08022224444"}
+  end
+end
